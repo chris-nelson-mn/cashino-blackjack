1b6ebc0866226536110cfeddc312d673ad99103f
Chris Nelson <chris.nelson.mn@gmail.com>
2016-10-28 23:59:39 -0500

    Add Card#to_s

--- app/lib/card.rb
+++ app/lib/card.rb
@@ -5,4 +5,8 @@ class Card
     @suit = suit
     @rank = rank
   end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
 end
--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Card do
-  subject { Card.new('spade', 'ace') }
+  subject { Card.new('spades', 'ace') }
 
   it 'returns the correct suit' do
-    expect(subject.suit).to eq('spade')
+    expect(subject.suit).to eq('spades')
   end
 
   it 'returns the correct rank' do
     expect(subject.rank).to eq('ace')
+  end
+
+  describe '#to_s' do
+    it do
+      expect(subject.to_s).to eq('ace of spades')
+    end
   end
 end
