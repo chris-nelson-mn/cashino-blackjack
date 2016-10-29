fea0ad44763077e892c52ec5aa4c1e66a047a4df
Chris Nelson <chris.nelson.mn@gmail.com>
2016-10-28 23:32:31 -0500

    Spec for Deck, initalize, shuffle, and draw

--- lib/autoloaded/deck.rb
+++ lib/autoloaded/deck.rb
@@ -1,2 +1,26 @@
 class Deck
+  DEFAULT_SUITS = %w(spades clubs diamonds hearts)
+  DEFAULT_RANKS = %w(ace one two three four five six seven eight nine ten jack queen king)
+
+  def initialize(suits, ranks)
+    @suits = suits
+    @ranks = ranks
+    @cards = Array.new
+
+    # Top of the deck is the last entry in the array
+    # Reversing so deck starts with cards in order from least to highest face value
+    suits.reverse.each do |suit|
+      ranks.reverse.each do |rank|
+        @cards << Card.new(suit, rank)
+      end
+    end
+  end
+
+  def draw
+    @cards.pop
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
 end
---
+++ spec/autoloaded/deck_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+require 'autoloaded/deck'
+
+RSpec.describe Deck do
+  let(:deck) { Deck.new(Deck::DEFAULT_SUITS, Deck::DEFAULT_RANKS) }
+
+  describe '.new' do
+    it 'returns a Deck when initialized with a set of suits and ranks' do
+      expect(deck).to be_a(Deck)
+    end
+  end
+
+  describe '#draw' do
+    let(:drawn_card) { deck.draw }
+    it 'returns a card, the ace of spades' do
+      expect(drawn_card).to be_a(Card)
+      expect(drawn_card.suit).to eq('spades')
+      expect(drawn_card.rank).to eq('ace')
+    end
+  end
+
+  describe '#shuffle' do
+    let(:drawn_card) { deck.draw }
+    before do
+      deck.shuffle
+    end
+
+    it 'reorders the cards' do
+      expect("#{drawn_card.rank} of #{drawn_card.suit}").to_not eq("ace of spades") 
+    end
+  end
+end
