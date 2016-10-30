c6091b5f1f984144495b753960e7f9064acad56a
Chris Nelson <chris.nelson.mn@gmail.com>
2016-10-30 00:27:30 -0500

    Add score class method to Blackjack

--- app/lib/blackjack.rb
+++ app/lib/blackjack.rb
@@ -1,4 +1,41 @@
 class Blackjack
+  class << self
+    def score(hand, options={})
+      options.reverse_merge({ aces_high: false })
+
+      hand.cards.map { |card| value_for(card, options[:aces_high]) }.sum
+    end
+
+    private
+
+    def value_for(card, ace_high)
+      case card.rank
+      when :ace
+        ace_high ? 11 : 1
+      when :two
+        2
+      when :three
+        3
+      when :four
+        4
+      when :five
+        5
+      when :six
+        6
+      when :seven
+        7
+      when :eight
+        8
+      when :nine
+        9
+      when :jack, :queen, :king
+        10
+      else
+        fail "#{card.rank} is not a legal card rank in Blackjack"
+      end
+    end
+  end
+
   def initialize(deck)
     @deck = deck
   end
--- spec/lib/blackjack_spec.rb
+++ spec/lib/blackjack_spec.rb
@@ -18,4 +18,35 @@ RSpec.describe Blackjack do
       expect(blackjack.deal).to be_a(Card)
     end
   end
+
+  describe '.score' do
+    subject { Blackjack.score(hand) }
+    context 'with a hand that has numeric cards' do
+      let(:hand) { Hand.new [Card.new(:eight, :clubs), Card.new(:five, :clubs)] }
+
+      it { is_expected.to eq(13) }
+    end
+
+    context 'with a hand that has face cards and numeric cards' do
+      let(:hand) { Hand.new [Card.new(:queen, :clubs), Card.new(:five, :clubs), Card.new(:king, :hearts)] }
+
+      it { is_expected.to eq(25) }
+    end
+
+    context 'with a hand that has an ace' do
+      let(:hand) { Hand.new [Card.new(:ace, :clubs), Card.new(:five, :diamonds)] }
+
+      context 'with default treatment of aces (scored as 1)' do
+        it { expect(Blackjack.score(hand)).to eq(6) }
+      end
+
+      context 'with aces treated as 1' do
+        it { expect(Blackjack.score(hand, aces_high: false)).to eq(6) }
+      end
+
+      context 'with aces treated as 11' do
+        it { expect(Blackjack.score(hand, aces_high: true)).to eq(16) }
+      end
+    end
+  end
 end
