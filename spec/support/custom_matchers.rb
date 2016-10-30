127100055fffca21bb0ca0c32fd010824e9105ec
Chris Nelson <chris.nelson.mn@gmail.com>
2016-10-29 22:56:34 -0500

    Add custom matcher for attr_accessor

---
+++ spec/support/custom_matchers.rb
@@ -0,0 +1,21 @@
+require 'rspec/expectations'
+
+# http://stackoverflow.com/a/28705325
+RSpec::Matchers.define :have_attr_accessor_for do |field|
+  match do |object_instance|
+    object_instance.respond_to?(field) &&
+      object_instance.respond_to?("#{field}=")
+  end
+
+  failure_message do |object_instance|
+    "expected attr_accessor for #{field} on #{object_instance}"
+  end
+
+  failure_message_when_negated do |object_instance|
+    "expected attr_accessor for #{field} not to be defined on #{object_instance}"
+  end
+
+  description do
+    "assert there is an attr_accessor for #{field}"
+  end
+end
