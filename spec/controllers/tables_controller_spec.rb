83c7b52eef1556cfe05893b7c66ee24efcc4d2be
Chris Nelson <chris.nelson.mn@gmail.com>
2016-10-31 03:13:49 -0500

    Add stub views, controller spec, routing spec

---
+++ spec/controllers/tables_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe TablesController, type: :controller do
+
+  describe "GET #index" do
+  end
+
+  describe "GET #show" do
+  end
+
+  describe "GET #new" do
+  end
+
+  describe "GET #edit" do
+  end
+
+  describe "POST #create" do
+  end
+
+  describe "PUT #update" do
+  end
+
+  describe "DELETE #destroy" do
+  end
+end
