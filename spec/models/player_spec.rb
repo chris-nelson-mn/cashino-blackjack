04bbf9c86c9a1b1fff3724828cec0bd980a93f2b
Chris Nelson <chris.nelson.mn@gmail.com>
2016-10-31 02:59:11 -0500

    Remove unused specs relating to Dealer, and a focus: true directive

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Player, type: :model, focus: true do
+RSpec.describe Player, type: :model do
 
   describe '.new' do
     it 'returns a Player instance' do
       expect(Player.new).to be_a(Player)
     end
   end
-
-  it { is_expected.to have_attr_accessor_for(:bet) }
-  it { is_expected.to have_attr_reader_for(:hands) }
 
   let(:deck) { Deck.new(shuffled: true) }
 
