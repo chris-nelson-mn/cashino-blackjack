2ffef6e52c0b1add5442cb7015a10ed5d872d202
Chris Nelson <chris.nelson.mn@gmail.com>
2016-10-31 01:47:33 -0500

    Remove spec for removed Dealer class

--- spec/lib/dealer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Dealer do
-  it { is_expected.to be_a_kind_of(Player) }
-
-  describe '.new' do
-    it { expect(Dealer.new).to be_instance_of(Dealer) }
-  end
-end
